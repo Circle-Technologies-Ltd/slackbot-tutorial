d63197b52e44abe8f41e3ad408d4ed9466e17787
Jamie Stirrat <jamiestirrat@hotmail.co.uk>
2022-01-12 15:50:04 +0000

    weekend bot init

--- scheduled.py
+++ scheduled.py
@@ -13,7 +13,7 @@ def sendMessage(slack_client, msg):
   # check if the request was a success
   try:
     slack_client.chat_postMessage(
-      channel='#test',
+      channel='#random',
       text=msg
     )#.get()
   except SlackApiError as e:
@@ -26,10 +26,9 @@ if __name__ == "__main__":
   logging.debug("authorized slack client")
 
   # # For testing
-  msg = "Good Morning!"
-  schedule.every(60).seconds.do(lambda: sendMessage(slack_client, msg))
-
-  # schedule.every().monday.at("13:15").do(lambda: sendMessage(slack_client, msg))
+  msg = "What have you been up to this weekend?"
+  # schedule.every(60).seconds.do(lambda: sendMessage(slack_client, msg))
+  schedule.every().monday.at("10:30").do(lambda: sendMessage(slack_client, msg))
   logging.info("entering loop")
 
   while True:
